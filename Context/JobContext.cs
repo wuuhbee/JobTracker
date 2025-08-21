f74ef48d3ba78f995f314231956be98ab5b99e78
Melyn Lim <engvid540@engcloudla.org>
2025-08-21 13:38:11 -0700

    Created Context Class and added implementation.

---
+++ Context/JobContext.cs
@@ -0,0 +1,15 @@
+﻿using JobTracker.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace JobTracker.JobContext
+{
+    public class JobContext : DbContext
+    {
+        public DbSet<Job> Jobs { get; set; }
+    
+        public JobContext(DbContextOptions<JobContext> options) :  base(options)
+        {
+        }
+    }
+}
