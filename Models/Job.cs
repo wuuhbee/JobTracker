691473b9e6ab7b6f56787eabcbd5d708552afe60
Melyn Lim <engvid540@engcloudla.org>
2025-08-25 11:25:05 -0700

    Changed display names to make more readable

--- Models/Job.cs
+++ Models/Job.cs
@@ -6,12 +6,15 @@ namespace JobTracker.Models
     {
         public int JobId { get; set; }
         [Required]
+        [Display(Name = "Job Title")]
         public string? JobTitle { get; set; }
 
+        [Display(Name = "Company Name")]
         public string? CompanyName { get; set; }
 
         public string? Status { get; set; }
 
+        [Display(Name = "Date Applied")]
         [DataType(DataType.Date)]
         public DateTime? DateApplied { get; set; }
     }
