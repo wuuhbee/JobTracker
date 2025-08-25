efddd3421e978fb4160e964ce51d5f0b8077524a
Melyn Lim <engvid540@engcloudla.org>
2025-08-25 15:48:42 -0700

    Added button to connect Jobs and Status tables for easy user access

--- Controllers/JobsController.cs
+++ Controllers/JobsController.cs
@@ -59,6 +59,11 @@ namespace JobTracker.Controllers
             ViewBag.Statuses = new SelectList(statuses, "Id", "StatusName");
         }
 
+        public IActionResult AddStatus()
+        {
+            return RedirectToAction("Index", "Status");
+        }
+
         // POST: Jobs/Create
         // To protect from overposting attacks, enable the specific properties you want to bind to.
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
