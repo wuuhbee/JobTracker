6a383c2aea87ee63156d9169fdb450797e9454a3
Melyn Lim <engvid540@engcloudla.org>
2025-08-25 11:22:13 -0700

    Change DateApplied type to DateTime for robustness

--- Models/Job.cs
+++ Models/Job.cs
@@ -1,15 +1,18 @@
-﻿namespace JobTracker.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
 {
     public class Job
     {
         public int JobId { get; set; }
-
+        [Required]
         public string? JobTitle { get; set; }
 
         public string? CompanyName { get; set; }
 
         public string? Status { get; set; }
 
-        public string? DateApplied { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime? DateApplied { get; set; }
     }
 }
