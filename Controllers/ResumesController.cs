f46a12f26b295de2efc055b1444696457c64df2b
Melyn Lim <engvid540@engcloudla.org>
2025-08-26 15:39:18 -0700

    Added files

---
+++ Controllers/ResumesController.cs
@@ -0,0 +1,157 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using JobTracker.Context;
+using JobTracker.Models;
+
+namespace JobTracker.Controllers
+{
+    public class ResumesController : Controller
+    {
+        private readonly JobContext _context;
+
+        public ResumesController(JobContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Resumes
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Resumes.ToListAsync());
+        }
+
+        // GET: Resumes/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+
+            return View(resume);
+        }
+
+        // GET: Resumes/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Resumes/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,DateAdded,FileName,FilePath")] Resume resume)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(resume);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resume);
+        }
+
+        // GET: Resumes/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes.FindAsync(id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+            return View(resume);
+        }
+
+        // POST: Resumes/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,DateAdded,FileName,FilePath")] Resume resume)
+        {
+            if (id != resume.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(resume);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!ResumeExists(resume.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(resume);
+        }
+
+        // GET: Resumes/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var resume = await _context.Resumes
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (resume == null)
+            {
+                return NotFound();
+            }
+
+            return View(resume);
+        }
+
+        // POST: Resumes/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var resume = await _context.Resumes.FindAsync(id);
+            if (resume != null)
+            {
+                _context.Resumes.Remove(resume);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool ResumeExists(int id)
+        {
+            return _context.Resumes.Any(e => e.Id == id);
+        }
+    }
+}
