d9e63aeca5ba548f0ff6611cca55bee2a2afd0da
Melyn Lim <engvid540@engcloudla.org>
2025-09-04 09:10:03 -0700

    Created new resume view model to handle the file uploads.

---
+++ Models/ResumeViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace JobTracker.Models
+{
+    public class ResumeViewModel
+    {
+        public string FileName { get; set; }
+
+        [Required]
+        public IFormFile ResumeFile { get; set; }
+    }
+}
