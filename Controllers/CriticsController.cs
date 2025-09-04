138f6d2de201e0246fce14dcd4d551313fc64597
Melyn Lim <engvid540@engcloudla.org>
2025-09-04 13:11:17 -0700

    Created a Critic Controller and a view to match and linked it to the nav bar

---
+++ Controllers/CriticsController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace JobTracker.Controllers
+{
+    public class CriticsController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
